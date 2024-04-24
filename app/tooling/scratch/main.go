59e45425be010566a9d8891cb6db0488fded58d9
Phyo Yarzar <phyoyazar592@gmail.com>
2024-04-24 20:43:24 +0630

    generate signature base on data and privateKey and take public key from the signature

--- app/tooling/scratch/main.go
+++ app/tooling/scratch/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"encoding/json"
 	"fmt"
 	"log"
 
@@ -14,14 +17,55 @@ func main() {
 }
 
 func run() error {
-	pk, err := crypto.GenerateKey()
+	// Need to load the private key file for the configured beneficiary so the
+	// account can get credited with fees and tips.
+	path := fmt.Sprintf("%s%s.ecdsa", "zblock/accounts/", "kennedy")
+	privateKey, err := crypto.LoadECDSA(path)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("unable to load private key for node: %w", err)
 	}
-	fmt.Println("private key: ", pk)
 
-	addr := crypto.PubkeyToAddress(pk.PublicKey).String()
+	addr := crypto.PubkeyToAddress(privateKey.PublicKey).String()
 	fmt.Println(addr)
+
+	v := struct {
+		Name string `json:"name"`
+	}{
+		Name: "Bill",
+	}
+
+	data, err := json.Marshal(v)
+	if err != nil {
+		return err
+	}
+
+	// Hash the transaction data into a 32 byte array. This will provide
+	// a data length consistency with all transactions.
+	txHash := crypto.Keccak256(data)
+
+	// Sign the hash with the private key to produce a signature.
+	sig, err := crypto.Sign(txHash, privateKey)
+	if err != nil {
+		return fmt.Errorf("signature error: %w", err)
+	}
+
+	fmt.Println("SIG: ", sig)
+
+	// =========================================================================
+
+	sigPublicKey, err := crypto.Ecrecover(txHash, sig)
+	if err != nil {
+		return err
+	}
+
+	// Capture the public key associated with this signature.
+	x, y := elliptic.Unmarshal(crypto.S256(), sigPublicKey)
+	publicKey := ecdsa.PublicKey{Curve: crypto.S256(), X: x, Y: y}
+
+	// Extract the account address from the public key.
+
+	address := crypto.PubkeyToAddress(publicKey).String()
+	fmt.Println(address)
 
 	return nil
 }
