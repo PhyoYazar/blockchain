61d67ab97b39d24186a9a0e55027a33f1a4ea925
Phyo Yarzar <phyoyazar592@gmail.com>
2024-04-25 18:33:32 +0630

    setup transaction and signature

--- foundation/blockchain/database/transaction.go
+++ foundation/blockchain/database/transaction.go
@@ -1,9 +1,13 @@
 package database
 
 import (
+	"bytes"
 	"crypto/ecdsa"
+	"encoding/hex"
 	"errors"
+	"fmt"
 	"math/big"
+	"time"
 
 	"github.com/PhyoYazar/blockchain/foundation/blockchain/signature"
 )
@@ -73,4 +77,90 @@ type SignedTx struct {
 	V *big.Int `json:"v"` // Ethereum: Recovery identifier, either 29 or 30 with ardanID.
 	R *big.Int `json:"r"` // Ethereum: First coordinate of the ECDSA signature.
 	S *big.Int `json:"s"` // Ethereum: Second coordinate of the ECDSA signature.
+}
+
+// Validate verifies the transaction has a proper signature that conforms to our
+// standards. It also checks the from field matches the account that signed the
+// transaction. Last it checks the format of the from and to fields.
+func (tx SignedTx) Validate(chainID uint16) error {
+	if tx.ChainID != chainID {
+		return fmt.Errorf("invalid chain id, got[%d] exp[%d]", tx.ChainID, chainID)
+	}
+
+	if !tx.FromID.IsAccountID() {
+		return errors.New("from account is not properly formatted")
+	}
+
+	if !tx.ToID.IsAccountID() {
+		return errors.New("to account is not properly formatted")
+	}
+
+	if tx.FromID == tx.ToID {
+		return fmt.Errorf("transaction invalid, sending money to yourself, from %s, to %s", tx.FromID, tx.ToID)
+	}
+
+	if err := signature.VerifySignature(tx.V, tx.R, tx.S); err != nil {
+		return err
+	}
+
+	address, err := signature.FromAddress(tx.Tx, tx.V, tx.R, tx.S)
+	if err != nil {
+		return err
+	}
+
+	if address != string(tx.FromID) {
+		return errors.New("signature address doesn't match from address")
+	}
+
+	return nil
+}
+
+// SignatureString returns the signature as a string.
+func (tx SignedTx) SignatureString() string {
+	return signature.SignatureString(tx.V, tx.R, tx.S)
+}
+
+// String implements the Stringer interface for logging.
+func (tx SignedTx) String() string {
+	return fmt.Sprintf("%s:%d", tx.FromID, tx.Nonce)
+}
+
+// =============================================================================
+
+// BlockTx represents the transaction as it's recorded inside a block. This
+// includes a timestamp and gas fees.
+type BlockTx struct {
+	SignedTx
+	TimeStamp uint64 `json:"timestamp"` // Ethereum: The time the transaction was received.
+	GasPrice  uint64 `json:"gas_price"` // Ethereum: The price of one unit of gas to be paid for fees.
+	GasUnits  uint64 `json:"gas_units"` // Ethereum: The number of units of gas used for this transaction.
+}
+
+// NewBlockTx constructs a new block transaction.
+func NewBlockTx(signedTx SignedTx, gasPrice uint64, unitsOfGas uint64) BlockTx {
+	return BlockTx{
+		SignedTx:  signedTx,
+		TimeStamp: uint64(time.Now().UTC().UnixMilli()),
+		GasPrice:  gasPrice,
+		GasUnits:  unitsOfGas,
+	}
+}
+
+// Hash implements the merkle Hashable interface for providing a hash
+// of a block transaction.
+func (tx BlockTx) Hash() ([]byte, error) {
+	str := signature.Hash(tx)
+
+	// Need to remove the 0x prefix from the hash.
+	return hex.DecodeString(str[2:])
+}
+
+// Equals implements the merkle Hashable interface for providing an equality
+// check between two block transactions. If the nonce and signatures are the
+// same, the two blocks are the same.
+func (tx BlockTx) Equals(otherTx BlockTx) bool {
+	txSig := signature.ToSignatureBytes(tx.V, tx.R, tx.S)
+	otherTxSig := signature.ToSignatureBytes(otherTx.V, otherTx.R, otherTx.S)
+
+	return tx.Nonce == otherTx.Nonce && bytes.Equal(txSig, otherTxSig)
 }
