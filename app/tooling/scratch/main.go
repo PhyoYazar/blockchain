b5253f3d2ac4cfeb7a350fa62042b31258a00319
Phyo Yarzar <phyoyazar592@gmail.com>
2024-04-26 21:06:20 +0630

    setup writing block into disk testing in scratch

--- app/tooling/scratch/main.go
+++ app/tooling/scratch/main.go
@@ -8,17 +8,112 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"time"
 
+	"github.com/PhyoYazar/blockchain/foundation/blockchain/database"
+	"github.com/PhyoYazar/blockchain/foundation/blockchain/merkle"
+	"github.com/PhyoYazar/blockchain/foundation/blockchain/signature"
+	"github.com/PhyoYazar/blockchain/foundation/blockchain/storage/disk"
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
 func main() {
-	err := sign()
+	// err := sign()
+	err := writeBlock()
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
+
+func writeBlock() error {
+	txs := []database.Tx{
+		{
+			ChainID: 1,
+			Nonce:   1,
+			FromID:  "0xdd6B972ffcc631a62CAE1BB9d80b7ff429c8ebA4",
+			ToID:    "0xF01813E4B85e178A83e29B8E7bF26BD830a25f32",
+			Value:   100,
+			Tip:     50,
+		},
+		{
+			ChainID: 1,
+			Nonce:   2,
+			FromID:  "0xdd6B972ffcc631a62CAE1BB9d80b7ff429c8ebA4",
+			ToID:    "0xF01813E4B85e178A83e29B8E7bF26BD830a25f32",
+			Value:   100,
+			Tip:     50,
+		},
+	}
+
+	blockTxs := make([]database.BlockTx, len(txs))
+
+	for i, tx := range txs {
+		blockTx, err := signToBlockTx(tx, 15)
+		if err != nil {
+			return err
+		}
+
+		blockTxs[i] = blockTx
+	}
+
+	// Construct a merkle tree from the transaction for this block. The root
+	// of this tree will be part of the block to be mined.
+	tree, err := merkle.NewTree(blockTxs)
+	if err != nil {
+		return err
+	}
+
+	beneficiaryID, err := database.ToAccountID("0xF01813E4B85e178A83e29B8E7bF26BD830a25f32")
+	if err != nil {
+		return err
+	}
+
+	// Construct the block to be mined.
+	block := database.Block{
+		Header: database.BlockHeader{
+			Number:        1,
+			PrevBlockHash: signature.ZeroHash,
+			TimeStamp:     uint64(time.Now().UTC().UnixMilli()),
+			BeneficiaryID: beneficiaryID,
+			Difficulty:    6,
+			MiningReward:  700,
+			StateRoot:     "not defined",
+			TransRoot:     tree.RootHex(), //
+			Nonce:         0,              // Will be identified by the POW algorithm.
+		},
+		MerkleTree: tree,
+	}
+
+	bd := database.NewBlockData(block)
+
+	d, err := disk.New("zblock/miner1")
+	if err != nil {
+		return nil
+	}
+
+	if err := d.Write(bd); err != nil {
+		return nil
+	}
+
+	return nil
+}
+
+func signToBlockTx(tx database.Tx, gas uint64) (database.BlockTx, error) {
+	pk, err := crypto.HexToECDSA("fae85851bdf5c9f49923722ce38f3c1defcfd3619ef5453230a58ad805499959")
+	if err != nil {
+		return database.BlockTx{}, err
+	}
+
+	signedTx, err := tx.Sign(pk)
+	if err != nil {
+		return database.BlockTx{}, err
+	}
+
+	return database.NewBlockTx(signedTx, gas, 1), nil
+}
+
+// =============================================================================
 
 func sign() error {
 	// Need to load the private key file for the configured beneficiary so the
